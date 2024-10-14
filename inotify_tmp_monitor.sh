551eea6a0a5d631b34632a603b933146db79d02d
saltchicken <john.eicher89@gmail.com>
2024-10-13 22:09:22 -0700

    Use journalctl via logger instead of making custom logfile

--- inotify_tmp_monitor.sh
+++ inotify_tmp_monitor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-LOGFILE="/var/log/inotify_tmp_monitor.log"
+# LOGFILE="/var/log/inotify_tmp_monitor_new.log"
 
 # Run inotifywait and log the output
 inotifywait -m -r -e create,modify,delete /tmp |
   while read path action file; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - File $file was $action in $path" >>"$LOGFILE"
+    logger -t "inotify_tmp_monitor_tag" "INFO File $file was $action in $path"
+    # TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+    # echo "$TIMESTAMP INFO File $file was $action in $path" >>"$LOGFILE"
   done
